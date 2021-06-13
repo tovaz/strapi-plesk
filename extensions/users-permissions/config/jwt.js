c384673a3e42c6a9939323c102fbab04bda41dce
root <root@focused-cohen.82-223-23-80.plesk.page>
2021-06-13 06:51:25 +0000

    First commit

---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ff4afdb6-7d24-4868-a3bb-af5f8634c6fe'
+};
---
+++ server.js
@@ -0,0 +1,16 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  url: env('URL', 'https://strapi.clever-yonath.82-223-23-80.plesk.page/'),
+  port: env.int('PORT', 1337),
+  //proxy: env('PROXY', true),
+  //NODE_ENV: 'development',
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd4e5c1fb58bde3a1d6433c7450944966'),
+    },
+  },
+});
+
+const strapi = require('strapi');
+
+strapi().start();
