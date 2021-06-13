cbfdecf6a17f9ca78781dd6be893d4e0092b267b
William Valdez <williamvaldez@outlook.com>
2021-06-13 12:29:02 +0200

    Added mysql config

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: "mysql",
-        host: env('DATABASE_HOST', '82.223.23.80'),
+        host: env('DATABASE_HOST', 'clever-yonath.82-223-23-80.plesk.page'),
         port: env('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'strapi'),
         username: env('DATABASE_USERNAME', 'strapi'),
